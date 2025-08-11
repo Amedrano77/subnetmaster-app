d3f57f16522dc79a4001864199c0aa6f84529eaa
Amedrano77 <amedrano5802@tstc.edu>
2025-08-10 21:50:43 -0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
 })
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-
-export default defineConfig({
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: "YOUR_ORG_SLUG",
-      project: "YOUR_PROJECT_SLUG",
-      authToken: process.env.SENTRY_AUTH_TOKEN, // set this in your env
-    }),
-  ],
-});
